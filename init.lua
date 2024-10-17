eec781849cea0e3f9c47516eeed01d773e2d2f37
Will Smith <undertakingyou@gmail.com>
2024-10-17 14:07:46 -0600

    Updates to help with typescript formatting and stuff

--- init.lua
+++ init.lua
@@ -110,6 +110,11 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Tabstops
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -801,7 +806,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
@@ -1023,7 +1028,11 @@ require('lazy').setup({
   {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
+    opts = {
+      baseIndentSize = 4,
+      indentSive = 4,
+      tabSize = 4,
+    },
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
