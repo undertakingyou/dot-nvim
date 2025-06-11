5546c5f5296afe7588bbb3b8830daf2c2738b909
Will Smith <undertakingyou@gmail.com>
2025-06-11 14:12:10 -0600

    Updates from May of 2025 (#4)
    * Get some things going for debug. Add oil.
    
    * Change default model
    
    * Add some things to try and add support for Vue.
    
    * Update for the latest provider configuration.

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -95,6 +95,8 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'node2',
+        'js',
       },
     }
 
@@ -142,6 +144,29 @@ return {
         -- On Windows delve must be run attached or it crashes.
         -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
         detached = vim.fn.has 'win32' == 0,
+      },
+    }
+
+    -- ╭──────────────────────────────────────────────────────────╮
+    -- │ Adapters                                                 │
+    -- ╰──────────────────────────────────────────────────────────╯
+    dap.adapters['node'] = {
+      type = 'server',
+      host = 'localhost',
+      port = '${port}',
+      executable = {
+        command = 'node',
+        args = { vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js', '${port}' },
+      },
+    }
+
+    dap.adapters['chrome'] = {
+      type = 'server',
+      host = 'localhost',
+      port = '${port}',
+      executable = {
+        command = 'node',
+        args = { vim.fn.stdpath 'data' .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js', '${port}' },
       },
     }
   end,
--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -6,4 +6,24 @@ return {
     indentSive = 4,
     tabSize = 4,
   },
+
+  config = function()
+    require('typescript-tools').setup {
+      filetypes = {
+        'javascript',
+        'javascriptreact',
+        'javascript.jsx',
+        'typescript',
+        'typescriptreact',
+        'typescript.tsx',
+        'vue',
+      },
+      settings = {
+        tsserver_plugins = {
+          -- Requires global install of '@vue/typescript-plugin' and '@vue/language-server'
+          '@vue/typescript-plugin',
+        },
+      },
+    }
+  end,
 }
