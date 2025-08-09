7f96b21e7bc19599b3c3691f281ec5f86d6c8535
Will Smith <undertakingyou@gmail.com>
2025-08-09 16:16:50 -0600

    Update config to avoid a warning

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -2,9 +2,14 @@
 
 return {
   'windwp/nvim-ts-autotag',
-  opts = {
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = true, -- Auto close on trailing </
-  },
+  config = function()
+    require('nvim-ts-autotag').setup({
+      opts = {
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = true, -- Auto close on trailing </
+      },
+      per_filetype = {}
+    })
+  end,
 }
