d97376236c9e7a04ea9b2a659dd65e286204f90b
Will Smith <undertakingyou@gmail.com>
2025-08-19 07:35:40 -0600

    Add path to the lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,6 +13,7 @@ return {
         theme = 'auto',
       },
       sections = {
+        lualine_c = { { 'filename', path = 1 } },
         lualine_x = {
           { 'filetype', separator = { right = nil, left = nil } },
           { 'fileformat', separator = { right = nil, left = nil } },
@@ -20,7 +21,7 @@ return {
         },
       },
       inactive_sections = {
-        lualine_a = { 'filename' },
+        lualine_a = { { 'filename', path = 1 } },
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
